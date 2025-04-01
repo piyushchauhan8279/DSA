dc0e468b05752dda463938f2b36c6b5662ff6b5c
Piyush Chauhan <piyushchauhan70900@gmail.com>
2025-04-01 22:58:07 +0530

    add

--- src/LinkedList/Doubly.java
+++ src/LinkedList/Doubly.java
@@ -60,6 +60,54 @@ public class Doubly {
 
     }
 
+    // function to add element in the middle of linkedlist:-
+
+    public void addMiddle(int data,int idx){
+        Node newNode =new Node(data);
+        size++;
+        if(head==null ){
+            head=tail=newNode;
+            return;
+        }
+        int i=0;
+        Node temp=head;
+        while(i<idx-1){
+            temp=temp.next;
+            i++;
+        }
+
+        newNode.next=temp.next;
+        temp.next.prev=newNode;
+        newNode.prev=temp;
+        temp.next=newNode;
+    }
+
+    // function to remove node form the starting of the linkedlist:-
+
+    public int removeFirst(){
+
+        if(head==null){
+            System.out.print("Empty Linked List");
+            return Integer.MAX_VALUE;
+        }
+        if(head.next==null){
+            head=null;
+            size=0;
+            return Integer.MAX_VALUE;
+
+        }
+        size--;
+        int val=head.data;
+        head=head.next;
+        head.prev=null;
+        return val;
+    }
+
+    //function to remove element from the end of the linkedlist:-
+
+
+
+
 
 
 
@@ -70,12 +118,29 @@ public class Doubly {
         dll.addFirst(10);
         dll.addFirst(15);
         dll.addLast(100);
+
+        dll.addMiddle(200,2);
+        dll.addMiddle(300,4);
         dll.print();
         System.out.println();
-        System.out.println(size);
+        System.out.println(dll.removeFirst());
 
-
-
-
+        dll.print();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
