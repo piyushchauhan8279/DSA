468cd9c1a9008c6e3a081be912c00dd66baaaf10
Piyush Chauhan <piyushchauhan70900@gmail.com>
2025-04-01 23:27:35 +0530

    add

--- src/LinkedList/Doubly.java
+++ src/LinkedList/Doubly.java
@@ -42,6 +42,7 @@ public class Doubly {
             curr=curr.next;
         }
         System.out.print("null");
+        System.out.println();
     }
 
     // function to add node in the last of double linkedlist:-
@@ -91,7 +92,7 @@ public class Doubly {
             return Integer.MAX_VALUE;
         }
         if(head.next==null){
-            head=null;
+            head=tail=null;
             size=0;
             return Integer.MAX_VALUE;
 
@@ -105,27 +106,90 @@ public class Doubly {
 
     //function to remove element from the end of the linkedlist:-
 
+    public int removeLast(){
+        if(head==null){
+            System.out.println("empty ll");
+            return Integer.MIN_VALUE;
+        }
+        else if(head.next==null){
+            head=tail=null;
+            return Integer.MIN_VALUE;
 
+        }
+        int val=tail.data;
+        tail=tail.prev;
+        tail.next=null;
+        return val;
+    }
 
+    // function to remove middle of a linked list:-
 
+    public int removeMiddle(int idx) {
+        if (head == null) {
+            System.out.println("Linked list is empty");
+            return Integer.MIN_VALUE;
+        }
 
+        // If removing the first node
+        if (idx == 0) {
+            return removeFirst();
+        }
+
+        Node temp = head;
+        int i = 0;
+
+        // Traverse to the node before the target index
+        while (i < idx - 1 && temp.next != null) {
+            temp = temp.next;
+            i++;
+        }
+
+        // Check if idx is out of bounds
+        if (temp.next == null) {
+            System.out.println("Index out of bounds");
+            return Integer.MIN_VALUE;
+        }
+
+        // If removing the last node
+        if (temp.next.next == null) {
+            return removeLast();
+        }
+
+        // Standard middle node removal
+        int val = temp.next.data;
+        temp.next = temp.next.next;
+        if (temp.next != null) {
+            temp.next.prev = temp; // Ensure `prev` is updated properly
+        }
+
+        return val;
+    }
 
 
     public static void main(String[] args) {
         Doubly dll=new Doubly();
-        dll.addFirst(10);
-        dll.addFirst(5);
-        dll.addFirst(10);
-        dll.addFirst(15);
-        dll.addLast(100);
+        dll.addFirst(2);
+        dll.addFirst(1);
 
-        dll.addMiddle(200,2);
-        dll.addMiddle(300,4);
+
+        dll.addLast(5);
+
+        dll.addMiddle(3,2);
+        dll.addMiddle(4,3);
         dll.print();
-        System.out.println();
+
         System.out.println(dll.removeFirst());
 
+
+        System.out.println(dll.removeLast());
+
         dll.print();
+
+        System.out.println(dll.removeMiddle(1));
+
+        dll.print();
+
+
     }
 }
 
